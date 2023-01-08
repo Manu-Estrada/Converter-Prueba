cb6df73f1f22437694f838d82064dd55923cb33e
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-01-08 21:41:54 +0100

    prueba

---
+++ vue-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="fondo">
+    <Navbar />
+    <Convertidor />
+  </div>
+</template>
+
+<script>
+import Convertidor from "./components/Convertidor";
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+  components: {
+    Convertidor,
+    Navbar,
+  },
+};
+</script>
+
+<style>
+.fondo {
+  background-image: url("./assets/euro-g6e55d6828_640.jpg");
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
